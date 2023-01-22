d97cfb8774ded2e2e9ab410d8c1288298fb0c6db
AnatolyRW <Ryzhkov.anatoly@gmail.com>
2023-01-22 23:22:10 +0500

    Ошибка

--- src/common/config/config.shema.ts
+++ src/common/config/config.shema.ts
@@ -3,24 +3,22 @@ import validator from 'convict-format-with-validator';
 
 convict.addFormats(validator);
 
-export type ConfigShema = {
+export type ConfigSchema = {
   PORT: number;
-  DB_HOST: string;
   SALT: string;
+  DB_HOST: string;
+  DB_USER: string;
+  DB_PASSWORD: string;
+  DB_PORT: number;
+  DB_NAME: string;
 }
 
-export const configShema = convict<ConfigShema>({
+export const configSchema = convict<ConfigSchema>({
   PORT: {
     doc: 'Port for incoming connections',
     format: 'port',
     env: 'PORT',
     default: 4000
-  },
-  DB_HOST: {
-    doc: 'Port for incoming connections',
-    format: 'ipaddress',
-    env: 'DB_HOST',
-    default: '127.0.0.1'
   },
   SALT: {
     doc: 'Salt for password hash',
@@ -28,4 +26,34 @@ export const configShema = convict<ConfigShema>({
     env: 'SALT',
     default: null
   },
+  DB_HOST: {
+    doc: 'IP address of the database server (MongoDB)',
+    format: 'ipaddress',
+    env: 'DB_HOST',
+    default: '127.0.0.1'
+  },
+  DB_USER: {
+    doc: 'Username to connect to the database (MongoDB)',
+    format: String,
+    env: 'DB_USER',
+    default: null,
+  },
+  DB_PASSWORD: {
+    doc: 'Database connection password (MongoDB)',
+    format: String,
+    env: 'DB_PASSWORD',
+    default: null,
+  },
+  DB_PORT: {
+    doc: 'Port to connect to the database (MongoDB)',
+    format: 'port',
+    env: 'DB_PORT',
+    default: 27017,
+  },
+  DB_NAME: {
+    doc: 'Database name (MongoDB)',
+    format: String,
+    env: 'DB_NAME',
+    default: 'course-nodejs-restapi'
+  }
 });
--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,14 @@ import ConfigService from './common/config/config.service.js';
 import { LoggerInterface } from './common/logger/logger.interface.js';
 import LoggerService from './common/logger/logger.service.js';
 import { Component } from './types/component.types.js';
+import DatabaseService from './common/database-client/database.service.js';
+import { DatabaseInterface } from './common/database-client/database.interface.js';
 
 const applicationContainer = new Container();
 applicationContainer.bind<AppLication>(Component.Application).to(AppLication).inSingletonScope();
 applicationContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService).inSingletonScope();
 applicationContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService).inSingletonScope();
+applicationContainer.bind<DatabaseInterface>(Component.DatabaseInterface).to(DatabaseService).inSingletonScope();
 
 const aplication = applicationContainer.get<AppLication>(Component.Application);
 await aplication.init();
